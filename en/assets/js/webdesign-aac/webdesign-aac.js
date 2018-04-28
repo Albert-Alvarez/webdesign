e3a04e963ec6e0c00037221d81908603adb6967c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-28 22:10:29 +0000

    Deploy Albert-Alvarez/webdesign to github.com/Albert-Alvarez/webdesign.git:gh-pages

---
+++ en/assets/js/webdesign-aac/webdesign-aac.js
@@ -0,0 +1,99 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 57)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: "Loading image #%curr%...",
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+})(jQuery); // End of use strict
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  new Typed('#typed3-1', {
+    strings: ["Hi!^500 I'm Albert^1000 and I'm here to help you with^500"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 2000,
+    smartBackspace: true,
+    loop: false,
+    onComplete: function () {
+      document.querySelectorAll(".typed-cursor")[0].style.display = "none";
+      new Typed('#typed3-2', {
+        strings: ["web design.", "user interface.", "Wordpress development."],
+        typeSpeed: 40,
+        backSpeed: 40,
+        backDelay: 3000,
+        smartBackspace: true,
+        loop: true
+      });
+  }
+  });
+
+}); 
